8c626e45835aadbdd0f5feed0e5f3f5dc831a4e6
kryvetskyi <kryvetskyi@gmail.com>
2023-07-03 18:18:20 +0300

    Added test for group modyfying.

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from fixture.group import GroupHelper
 from fixture.session import SessionHelper
 
 
@@ -8,33 +9,10 @@ class Application:
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         self.driver.get("http://localhost:8888/addressbook/")
-
-    def open_groups_page(self):
-        self.driver.find_element(By.LINK_TEXT, "groups").click()
-
-    def create_group(self, group):
-        self.open_groups_page()
-
-        # init group creation
-        self.driver.find_element(By.NAME, "new").click()
-
-        # fill group form
-        self.driver.find_element(By.NAME, "group_name").click()
-        self.driver.find_element(By.NAME, "group_name").send_keys(group.name)
-        self.driver.find_element(By.NAME, "group_header").click()
-        self.driver.find_element(By.NAME, "group_header").send_keys(group.header)
-        self.driver.find_element(By.NAME, "group_footer").click()
-        self.driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
-
-        # submit group creation
-        self.driver.find_element(By.NAME, "submit").click()
-        self.return_to_groups_page()
-
-    def return_to_groups_page(self):
-        self.driver.find_element(By.LINK_TEXT, "group page").click()
 
     def destroy(self):
         self.driver.quit()
