ae8e715429114a880e3452a3ff8e5b84fc0c9548
kryvetskyi <kryvetskyi@gmail.com>
2023-07-04 16:34:31 +0300

    Added validator for login.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,32 +2,27 @@ import pytest
 
 from fixture.application import Application
 
-
 fixture = None
 
 
 @pytest.fixture
 def app():
     global fixture
-    if fixture is None:
+    if fixture is None or (not fixture.is_valid()):
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
 
-    # Check if session is valid
-    if not fixture.is_valid():
-        fixture = Application()
-        fixture.session.login(username="admin", password="secret")
+    fixture.session.ensure_login(username="admin", password="secret")
 
     yield fixture
 
 
 @pytest.fixture(scope="session", autouse=True)
 def logout_and_destroy(request):
+    fixture = Application()
+
     def finalizer():
-        global fixture
-        if fixture is None:
-            fixture = Application()
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
 
     request.addfinalizer(finalizer)
+    yield fixture
